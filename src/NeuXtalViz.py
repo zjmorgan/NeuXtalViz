15be1a0be913df40268575a848cd8b94a5b01e18
Zachary Morgan (zgf) <zjmorgan@mtu.edu>
2025-01-18 09:22:59 -0500

    Update process

--- src/NeuXtalViz.py
+++ src/NeuXtalViz.py
@@ -159,7 +159,9 @@ class NeuXtalViz(QMainWindow):
             main_py_path = os.path.join(directory, "main.py")
             if os.path.isfile(main_py_path):
                 try:
-                    subprocess.run(["mantidpython", main_py_path], check=True)
+                    subprocess.Popen(
+                        ["mantidpython", main_py_path], check=True
+                    )
                 except subprocess.CalledProcessError as e:
                     QMessageBox.critical(
                         self, "Error", f"Failed to execute main.py:\n{e}"
