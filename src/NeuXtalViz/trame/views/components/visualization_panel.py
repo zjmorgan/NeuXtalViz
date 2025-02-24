d208cbf61733b3d2ad7f16189d22fccfa38dfd5f
John Duggan <dugganjw@ornl.gov>
2025-02-24 15:24:11 -0500

    Add remaining camera controls

--- src/NeuXtalViz/trame/views/components/visualization_panel.py
+++ src/NeuXtalViz/trame/views/components/visualization_panel.py
@@ -1,8 +1,10 @@
+import numpy as np
 import pyvista as pv
+from io import BytesIO
 from nova.trame.view.components import InputField
 from nova.trame.view.layouts import GridLayout, VBoxLayout
 from pyvista.trame.ui import get_viewer
-from trame.widgets import html
+from trame.widgets import client, html
 from trame.widgets import vuetify3 as vuetify
 
 
@@ -16,12 +18,17 @@ class VisualizationPanel:
         self.view_model.progress_bind.connect("progress")
         self.view_model.status_bind.connect("status")
         self.view_model.update_processing("Ready!", 0)
+        self.view_model.up_vector_bind.connect(self.view_up_vector)
+        self.view_model.vector_bind.connect(self.view_vector)
 
         # These are just for compatibility with the view model as it communicates with Qt slightly differently
         self.view_model.lattice_parameters_bind.connect("lattice_parameters")
 
         self.camera_position = None
         self.plotter = self.create_plotter()
+        self.js_download = client.JSEval(
+            exec="utils.download('neuxtalviz.png', $event, 'image/png')"
+        ).exec
 
         self.create_ui()
 
@@ -39,7 +46,7 @@ class VisualizationPanel:
         with vuetify.VContainer(classes="mr-2 pa-0", fluid=True):
             with GridLayout(columns=5):
                 with VBoxLayout(valign="space-between"):
-                    vuetify.VBtn("Save Screenshot")
+                    vuetify.VBtn("Save Screenshot", click=self.save_screenshot)
                     vuetify.VBtn("Reset View", click=self.reset_view)
                     vuetify.VBtn("Reset Camera", click=self.reset_camera)
 
@@ -57,15 +64,15 @@ class VisualizationPanel:
                                 vuetify.VBtn("+Qx", click=self.plotter.view_yz)
                                 vuetify.VBtn("+Qy", click=self.plotter.view_zx)
                                 vuetify.VBtn("+Qz", click=self.plotter.view_xy)
-                                vuetify.VBtn("a*")
-                                vuetify.VBtn("b*")
-                                vuetify.VBtn("c*")
+                                vuetify.VBtn("a*", click=self.view_model.view_bc_star)
+                                vuetify.VBtn("b*", click=self.view_model.view_ca_star)
+                                vuetify.VBtn("c*", click=self.view_model.view_ab_star)
                                 vuetify.VBtn("-Qx", click=self.plotter.view_zy)
                                 vuetify.VBtn("-Qy", click=self.plotter.view_xz)
                                 vuetify.VBtn("-Qz", click=self.plotter.view_yx)
-                                vuetify.VBtn("a")
-                                vuetify.VBtn("b")
-                                vuetify.VBtn("c")
+                                vuetify.VBtn("a", click=self.view_model.view_bc)
+                                vuetify.VBtn("b", click=self.view_model.view_ca)
+                                vuetify.VBtn("c", click=self.view_model.view_ab)
                         with vuetify.VWindowItem(value=2):
                             with GridLayout(columns=10, halign="center"):
                                 vuetify.VLabel("h")
@@ -79,11 +86,19 @@ class VisualizationPanel:
                                 InputField(v_model="controls.manual_axes[0]")
                                 InputField(v_model="controls.manual_axes[1]")
                                 InputField(v_model="controls.manual_axes[2]")
-                                vuetify.VBtn("View Axis", column_span=2)
+                                vuetify.VBtn(
+                                    "View Axis",
+                                    column_span=2,
+                                    click=self.view_model.view_manual,
+                                )
                                 InputField(v_model="controls.manual_up_axes[0]")
                                 InputField(v_model="controls.manual_up_axes[1]")
                                 InputField(v_model="controls.manual_up_axes[2]")
-                                vuetify.VBtn("View Up Axis", column_span=2)
+                                vuetify.VBtn(
+                                    "View Up Axis",
+                                    column_span=2,
+                                    click=self.view_model.view_up_manual,
+                                )
 
                 with VBoxLayout(valign="space-between"):
                     InputField(v_model="controls.reciprocal_lattice", type="checkbox")
@@ -100,6 +115,12 @@ class VisualizationPanel:
                 striped=("progress < 100",),
             ):
                 html.Span("{{ status }}")
+
+    def save_screenshot(self):
+        data = BytesIO()
+        self.plotter.screenshot(data)
+        data.seek(0)
+        self.js_download(data.read())
 
     def clear_scene(self):
         self.plotter.clear_plane_widgets()
@@ -158,3 +179,13 @@ class VisualizationPanel:
             self.plotter.enable_parallel_projection()
         else:
             self.plotter.disable_parallel_projection()
+
+    def view_vector(self, vecs):
+        if len(vecs) == 2:
+            vec = np.cross(vecs[0], vecs[1])
+            self.plotter.view_vector(vecs[0], vec)
+        else:
+            self.plotter.view_vector(vecs)
+
+    def view_up_vector(self, vec):
+        self.plotter.set_viewup(vec)
